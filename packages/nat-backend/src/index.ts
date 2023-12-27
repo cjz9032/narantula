1673060a0912afea7579d56f01f18da80d0840ad
Tanner <402821051@qq.com>
2023-12-28 02:04:33 +0800

    feat: add images

--- packages/nat-backend/src/index.ts
+++ packages/nat-backend/src/index.ts
@@ -8,13 +8,15 @@ import {
 } from "selenium-webdriver";
 import { parseUrl } from "./url";
 import path from "path";
+import fetch from "node-fetch";
+import fs from "fs";
 import { writeListToCSV } from "./simple-csv";
 import { fileURLToPath } from "url";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
-const MAX_A_LINK = 100;
+const MAX_ENTRIES = 30;
 void (async function main() {
   const driver = await new Builder().forBrowser(Browser.CHROME).build();
 
@@ -27,7 +29,7 @@ void (async function main() {
     );
     const elements = (await driver.findElements(By.css("a"))).slice(
       0,
-      MAX_A_LINK,
+      MAX_ENTRIES,
     );
     const result: string[][] = [];
     for (const element of elements) {
@@ -38,8 +40,44 @@ void (async function main() {
         result.push([text, url.href]);
       }
     }
-    
-    writeListToCSV(["text", "url"],result, path.join(dirname, "../public/output.csv"));
+
+    writeListToCSV(
+      ["text", "url"],
+      result,
+      path.join(dirname, "../public/output.csv"),
+    );
+    fs.writeFileSync(
+      path.join(dirname, "../public/output.txt"),
+      result.map((t) => t.join(",")).join("\n"),
+    );
+
+    const imgElements = (await driver.findElements(By.css("img"))).slice(
+      0,
+      MAX_ENTRIES,
+    );
+    const imgResults: URL[] = [];
+    for (const element of imgElements) {
+      const link = await element.getAttribute("src");
+      const url = parseUrl(link);
+
+      if (url) {
+        const ext = path.extname(url.href).replace('.', '');
+        if (["jpg", "jpeg", "png", "gif"].includes(ext.toLocaleLowerCase()))
+          imgResults.push(url);
+      }
+    }
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    imgResults.forEach(async (imageUrl) => {
+      const response = await fetch(imageUrl.href);
+      const arrayBuffer = await response.arrayBuffer();
+      const fileName = path.basename(imageUrl.pathname);
+      const imagePath = path.join(
+        dirname,
+        `../public/imgs/${fileName}`,
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      fs.writeFileSync(imagePath, Buffer.from(arrayBuffer));
+    });
   } catch (e) {
     console.error(e);
   } finally {
--- packages/nat-ui/src/App.tsx
+++ packages/nat-ui/src/App.tsx
@@ -1,6 +1,18 @@
 import alinks from "@narantula/backend/public/output.csv";
 import "./App.css";
 
+const imagesRecord = import.meta.glob("../../nat-backend/public/imgs/*.*", {
+  eager: true,
+});
+const images: string[] = Object.values(imagesRecord).map(t=>t.default);
+
+
+// 遍历所有图片
+// for (const path in images) {
+//   // 在这里可以对导入的图片进行操作
+//   console.log(images[path]);
+// }
+
 function App() {
   return (
     <>
@@ -12,10 +24,17 @@ function App() {
           }[]
         ).map((link) => (
           <li>
-            <a target="_blank" href={link.url}>{link.text}</a>
+            <a target="_blank" href={link.url}>
+              {link.text}
+            </a>
           </li>
         ))}
       </ul>
+      <div>
+        {images.map((image) => (
+          <img width={50} src={image} alt="" />
+        ))}
+      </div>
     </>
   );
 }
